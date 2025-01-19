b7dc9c636801d5b638e559f664a2cd3c3090a5a8
IceboxDev <mantas_kandratavicius@yahoo.com>
2025-01-18 21:25:59 -0800

    fixed setup.sh again again again

--- setup.sh
+++ setup.sh
@@ -2,8 +2,14 @@ curl -o golang.pkg https://dl.google.com/go/go1.21.3.darwin-amd64.pkg
 sudo open golang.pkg
 rm golang.pkg
 
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$HOME/go/bin
+export PATH=$PATH:$GOPATH/bin
+
 echo "export GOROOT=/usr/local/go" >> .bash_profile
 echo "export GOPATH=$HOME/Documents/go" >> .bash_profile
 echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> .bash_profile
+
+source ~/.bash_profile
 
 go install github.com/desertbit/grml@latest
