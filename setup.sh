0dc18a50b8cc4d9f29fcc7c3fb2efd7fd42a409d
IceboxDev <mantas_kandratavicius@yahoo.com>
2025-01-18 21:35:47 -0800

    cleaned up merge issues

--- setup.sh
+++ setup.sh
@@ -10,15 +10,10 @@ echo "export GOROOT=/usr/local/go" >> .bash_profile
 echo "export GOPATH=$HOME/Documents/go" >> .bash_profile
 echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> .bash_profile
 
-<<<<<<< Updated upstream
 source ~/.bash_profile
-<<<<<<< Updated upstream
 source ~/.zshrc
-=======
-=======
+
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:$GOPATH/bin
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 
 go install github.com/desertbit/grml@latest
